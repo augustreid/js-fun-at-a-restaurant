aeec2c731eb1ce8d5d2461c6a4fdcada5c18b8a4
August Reid <c.august.reid@gmail.com>
2021-08-27 20:36:16 -0600

    Pass meal tests

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,44 @@
-function nameMenuItem() {
+function nameMenuItem(foodName) {
+return `Delicious ${foodName}`;
+};
 
+function createMenuItem(newItemName, itemPrice, itemType) {
+  var newItem = {
+    name: newItemName,
+    price: itemPrice,
+    type: itemType
+  };
+  return newItem;
+};
+
+function addIngredients(newIngredient, ingredients) {
+  if (ingredients.indexOf(newIngredient) === -1) {ingredients.push(newIngredient);
+};
+  return ingredients;
+};
+
+function formatPrice(itemPrice) {
+  return "$" + itemPrice;
+};
+
+function decreasePrice(itemPrice) {
+  return itemPrice * .9;
+};
+
+function createRecipe(title, ingredients, itemType) {
+  var recipe = {
+    title: title,
+    ingredients: ingredients,
+    type: itemType
+  };
+  return recipe;
 }
-
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
